e2bceed660196a1363ae15054156e1632fd226fc
daniel <daniel.smigiela@outlook.com>
2021-09-18 19:45:53 +0200

    create function to show all posts on view

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
 
 class PostController extends Controller
 {
@@ -15,85 +13,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Http::get('https://jsonplaceholder.typicode.com/posts')->collect();
-
-        foreach($posts as $post){
-            Post::updateOrCreate(
-                ['id' => $post['id']],
-                [
-                    'title' => $post['title'],
-                    'body' => $post['body'],
-                    'user_id' => $post['userId']
-                ]
-            );
-        }
+        $posts = Post::with('user')->orderByDesc('created_at')->paginate(20);
 
         return view('posts.index', compact('posts'));
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Post $post)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Post $post)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Post $post)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Post $post)
-    {
-        //
     }
 }
