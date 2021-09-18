3838708b79615b08f35cba3fec089c5981d4def3
daniel <daniel.smigiela@outlook.com>
2021-09-18 19:46:12 +0200

    refactor routes/web

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('posts', [PostController::class, 'index'])->name('posts.index');
+Route::resource('/users', UserController::class)->only(['index', 'show']);
